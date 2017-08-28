90209ab847b1e0c4bcfa27b25832fe8106ec736b
Ascher123 <30418759+Ascher123@users.noreply.github.com>
2017-08-28 16:48:03 -0400

    Add files via upload

--- TCGTradingPost/src/main/java/com/tcgtp/controllers/HomeController.java
+++ TCGTradingPost/src/main/java/com/tcgtp/controllers/HomeController.java
@@ -2,11 +2,23 @@ package com.tcgtp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
  
 @Controller
 public class HomeController {
 	@RequestMapping("/")
 	String home() {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	String login() {
 		return "Login";
 	}
+	
+	@RequestMapping("/cardsearch")
+	String cardSearch() {
+		return "CardSearch";
+	}
 }
+
