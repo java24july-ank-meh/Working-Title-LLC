773567abe0ff900b81b0c44a0809981aef3dd49a
Ascher123 <hichelt@gmail.com>
2017-08-31 10:56:39 -0400

    fixed orderHistory

--- TCGTradingPost/src/main/java/com/tcgtp/controllers/OrderHistoryController.java
+++ TCGTradingPost/src/main/java/com/tcgtp/controllers/OrderHistoryController.java
@@ -1,6 +1,5 @@
 package com.tcgtp.controllers;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
@@ -37,6 +33,7 @@ public class OrderHistoryController {
 	public void setOrderService(OrderService orderService) {
 		this.orderService = orderService;
 	}
+	@RequestMapping(value="/orderHistory", method=RequestMethod.GET)
 public ResponseEntity<String> orderHistory(Authentication authentication){
 	Gson gson = new Gson();
 	
