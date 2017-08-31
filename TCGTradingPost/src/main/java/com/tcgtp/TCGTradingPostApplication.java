bf2ed7b61b6c23372cf0f255a0495d5d075d0646
Ascher123 <hichelt@gmail.com>
2017-08-31 13:30:37 -0400

    trying to get deployment to work

--- TCGTradingPost/src/main/java/com/tcgtp/TCGTradingPostApplication.java
+++ TCGTradingPost/src/main/java/com/tcgtp/TCGTradingPostApplication.java
@@ -2,10 +2,17 @@ package com.tcgtp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TCGTradingPostApplication {
+public class TCGTradingPostApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TCGTradingPostApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TCGTradingPostApplication.class, args);
 	}
