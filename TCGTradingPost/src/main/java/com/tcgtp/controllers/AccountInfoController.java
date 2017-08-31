173c4a99f6c6e93347687a7adcb6a665ad210875
Vince Commero <vacommero@yahoo.com>
2017-08-31 10:46:11 -0400

    account info controller

--- TCGTradingPost/src/main/java/com/tcgtp/controllers/AccountInfoController.java
+++ TCGTradingPost/src/main/java/com/tcgtp/controllers/AccountInfoController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,12 +36,21 @@ public class AccountInfoController {
 		
 		JsonObject json = new JsonObject();
 		
-		List<User> userList = userService.listAll();
+		User currentUser = null;
+		for (User usr : userService.listAll()) {
+			if (authentication.getName().equals(usr.getUsername())) {
+				currentUser = usr;
+				break;
+			}
+		}
+		
+		json.addProperty("username", currentUser.getUsername());
+		json.addProperty("email", currentUser.getEmail());
+		json.addProperty("role", gson.toJson(currentUser.getRoles()));
+		json.addProperty("account_status", currentUser.getEnabled().toString());
 		
 		
-		
-		
-		return new ResponseEntity<String>(gson.toJson(sdfgsdg) , HttpStatus.OK);
+		return new ResponseEntity<String>(gson.toJson(json) , HttpStatus.OK);
 	}
 	
 }
